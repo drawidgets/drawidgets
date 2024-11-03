ade6c905ab3ffd60674facf5914f33c4027968b7
James Aprosail <aprosail@outlook.com>
2024-11-04 01:34:54 +0800

    feat(flutter): color map

--- lib/src/decorate.dart
+++ lib/src/decorate.dart
@@ -39,3 +39,32 @@ class Foreground extends StatelessWidget {
     );
   }
 }
+
+/// A collection of useful colors.
+///
+/// This `extension` is originally an `abstract final class`,
+/// like the `Colors` class with the same name in the `material` library.
+/// This is designed for convenience in common code editors
+/// including VSCode and Android Studio,
+/// because the code completion is much more slower when using a class here.
+/// All the fields are `static const` so there's no difference
+/// between their usage and even after compile.
+extension Colors on Color {
+  static const transparent = Color(0x00000000);
+
+  // Mono colors.
+  static const white = Color(0xffffffff);
+  static const snow = Color(0xfffdfeff);
+  static const paper = Color(0xfff7f6f5);
+  static const lunar = Color(0xffecebea);
+  static const chalk = Color(0xffcecdcc);
+  static const gray = Color(0xff7f7f7f);
+  static const ink = Color(0xff454647);
+  static const night = Color(0xff232425);
+  static const blackboard = Color(0xff101211);
+  static const coal = Color(0xff0c0b0b);
+  static const black = Color(0xff000000);
+
+  // Accent colors.
+  static const brick = Color(0xff804044);
+}
